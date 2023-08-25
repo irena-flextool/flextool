ed4b403266334f58c0e7ae6fc287b8b961ddd633
Antti Soininen <antti.soininen@vtt.fi>
2023-08-25 16:27:06 +0300

    Make the update script Web interface friendly
    We don't want to unconditianally git pull projects in the we interface.

--- update_flextool.py
+++ update_flextool.py
@@ -1,3 +1,4 @@
+import argparse
 import json
 import os
 import subprocess
@@ -6,43 +7,55 @@ import shutil
 from migrate_database import migrate_database
 
 
-def update_flextool():
+def update_flextool(skip_git):
 
-    shutil.copy('./.spinetoolbox/project.json','./.spinetoolbox/project_temp.json')
-    completed = subprocess.run(["git","restore","."])
-    completed = subprocess.run(["git","pull"])
-    shutil.copy('./.spinetoolbox/project_temp.json','./.spinetoolbox/project.json')
-    os.remove('./.spinetoolbox/project_temp.json')
+    shutil.copy("./.spinetoolbox/project.json", "./.spinetoolbox/project_temp.json")
+    if not skip_git:
+        completed = subprocess.run(["git", "restore", "."])
+        if completed.returncode != 0:
+            print("Failed to restore version controlled files.")
+            exit(-1)
 
-    if completed.returncode != 0:
-        print("Failed to get the new version")
-        exit(-1)
+        completed = subprocess.run(["git", "pull"])
+        if completed.returncode != 0:
+            print("Failed to get the new version.")
+            exit(-1)
 
-    if not os.path.exists('Input_data.sqlite'):
-        shutil.copy('input_data_template.sqlite','Input_data.sqlite')
-    if not os.path.exists('Results.sqlite'):
-        shutil.copy('Results_template.sqlite','Results.sqlite')
+    shutil.copy("./.spinetoolbox/project_temp.json", "./.spinetoolbox/project.json")
+    os.remove("./.spinetoolbox/project_temp.json")
+
+    if not os.path.exists("Input_data.sqlite"):
+        shutil.copy("input_data_template.sqlite", "Input_data.sqlite")
+    if not os.path.exists("Results.sqlite"):
+        shutil.copy("Results_template.sqlite", "Results.sqlite")
 
     db_to_update = []
-    db_to_update.append('init.sqlite')
-    db_to_update.append('input_data_template.sqlite')
-    
-    #add the database used in the input_data tool
-    with open ('./.spinetoolbox/project.json') as json_file:
-        specifications = json.load(json_file) 
-    path = specifications['items']['Input_data']['url']['database']['path']
+    db_to_update.append("init.sqlite")
+    db_to_update.append("input_data_template.sqlite")
+
+    # add the database used in the input_data tool
+    with open("./.spinetoolbox/project.json") as json_file:
+        specifications = json.load(json_file)
+    path = specifications["items"]["Input_data"]["url"]["database"]["path"]
     db_to_update.append(path)
 
-    #add the databases in the example folder
-    dir_list = os.listdir('./how to example databases')
+    # add the databases in the example folder
+    dir_list = os.listdir("./how to example databases")
     for i in dir_list:
         if i.endswith(".sqlite"):
-            db_to_update.append("how to example databases/"+ i)
+            db_to_update.append("how to example databases/" + i)
 
-    #migrate the databases to new version
+    # migrate the databases to new version
     for i in db_to_update:
         migrate_database(i)
 
 
-if __name__ == '__main__':
-    update_flextool()
+if __name__ == "__main__":
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument(
+        "--skip-git",
+        action="store_true",
+        help="skip 'git restore' and 'git pull' steps",
+    )
+    args = arg_parser.parse_args()
+    update_flextool(args.skip_git)
