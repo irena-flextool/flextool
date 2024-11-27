bb12f794f976dc971cb4f3d5d12e83690c9e6744
jkiviluo <Juha.Kiviluoma@vtt.fi>
2024-11-27 09:07:23 +0200

    Making flextool into pip package (for bundle installation at this point)

--- flextool/__init__.py
+++ flextool/__init__.py
@@ -1,4 +1,12 @@
 # Import specific items from submodules
 from .flextoolrunner import FlexToolRunner
+from .update_database import update_database
 from .migrate_database import migrate_database
 from .initialize_database import initialize_database
+
+__all__ = [
+    FlexToolRunner
+    update_database
+    migrate_database
+    initialize_database
+]
