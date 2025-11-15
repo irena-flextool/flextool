0649f4740721ee7176d028d438db6e350f520e75
jkiviluo <juha.kiviluoma@nodal-tools.fi>
2025-11-15 18:06:02 +0200

    Add scenario_comparison.json specification

--- flextool/__init__.py
+++ flextool/__init__.py
@@ -1,11 +1,13 @@
+from .write_outputs import write_outputs
+
 __all__ = [
     'FlexToolRunner',
     'update_flextool',
     'migrate_database',
     'initialize_database',
     'read_flextool_results',
-    'write_flextool_csv.py',
-    'process_results.py',
+    'process_results',
+    'write_outputs',
 ]
 
 name = "flextool"
